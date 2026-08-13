204d3681b8f71719ffc64e0867eb58c8fc99cc37
STHSCare12 <selina.sths@gmail.com>
2026-08-13 09:48:33 +0800

    Added BIA measurement in Anthropometry page

--- Backend/modules/Consultation/anthropometry/controller.js
+++ Backend/modules/Consultation/anthropometry/controller.js
@@ -1,8 +1,63 @@
 import {
+    calculateAdexAnthropometry,
+    createAdexAnthropometry,
+    getAdexAnthropometriesByAthleteId,
+    getAdexAnthropometryById,
+    getBiaMeasurementsByAthleteId,
     getAnthropometryBySessionId,
     patchAnthropometryBySessionId,
   } from "./services.js";
-  
+
+
+  export async function calculateAdexAnthropometryData(req, res, next) {
+    try {
+      const data = await calculateAdexAnthropometry(req.params.athleteId, req.body);
+      return res.status(200).json({ data });
+    } catch (err) {
+      return next(err);
+    }
+  }
+
+
+  export async function createAdexAnthropometryData(req, res, next) {
+    try {
+      const data = await createAdexAnthropometry(req.params.athleteId, req.body);
+      return res.status(201).json({ data });
+    } catch (err) {
+      return next(err);
+    }
+  }
+
+
+  export async function getBiaMeasurements(req, res, next) {
+    try {
+      const data = await getBiaMeasurementsByAthleteId(req.params.athleteId);
+      return res.status(200).json({ data });
+    } catch (err) {
+      return next(err);
+    }
+  }
+
+
+  export async function getAdexAnthropometries(req, res, next) {
+    try {
+      const data = await getAdexAnthropometriesByAthleteId(req.params.athleteId);
+      return res.status(200).json({ data });
+    } catch (err) {
+      return next(err);
+    }
+  }
+
+
+  export async function getAdexAnthropometryDetail(req, res, next) {
+    try {
+      const data = await getAdexAnthropometryById(req.params.anthropometryId);
+      return res.status(200).json({ data });
+    } catch (err) {
+      return next(err);
+    }
+  }
+ 
   export async function getAnthropometry(req, res, next) {
     try {
       const { sessionId } = req.params;
@@ -12,7 +67,7 @@ import {
       return next(err);
     }
   }
-  
+ 
   export async function patchAnthropometry(req, res, next) {
     try {
       const { sessionId } = req.params;
